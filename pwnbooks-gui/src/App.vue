e94f13cdb5f336e81a5d9070ea5f7bca3ddf850d
Str4thus <trashytrashmail42@gmail.com>
2021-02-17 23:36:15 +0100

    working routing navigation via sidebar menu

--- pwnbooks-gui/src/App.vue
+++ pwnbooks-gui/src/App.vue
@@ -1,32 +1,107 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" :class="{ collapsed: collapsed }">
+    <div class="app">
+      <div class="container">
+        <router-view />
+      </div>
+
+      <sidebar-menu
+        :menu="menu"
+        :collapsed="collapsed"
+        @toggle-collapse="onToggleCollapse"
+        :show-one-child="true"
+      />
+
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "Getting Started",
+          hiddenOnCollapse: true,
+        },
+        {
+          href: "/",
+          title: "Home",
+          icon: "fa fa-download",
+        },
+        {
+          href: "/about",
+          title: "About",
+          icon: "fa fa-download",
+        },
+      ],
+      collapsed: false,
+    };
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener("resize", this.onResize);
+  },
+  methods: {
+    onToggleCollapse(collapsed) {
+      console.log(collapsed);
+      this.collapsed = collapsed;
+    },
+    onResize() {
+      if (window.innerWidth <= 767) {
+        this.collapsed = true;
+      } else {
+        this.collapsed = false;
+      }
+    },
+  },
+};
+</script>
+
 <style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 18px;
+  background-color: #f2f4f7;
+  color: #262626;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding-left: 350px;
+  transition: 0.3s ease;
 }
-
-#nav {
-  padding: 30px;
+#app.collapsed {
+  padding-left: 50px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.sidebar-overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: #000;
+  opacity: 0.5;
+  z-index: 900;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 50px;
+}
+.container {
+  max-width: 900px;
+}
+pre {
+  font-family: Consolas, monospace;
+  color: #000;
+  background: #fff;
+  border-radius: 2px;
+  padding: 15px;
+  line-height: 1.5;
+  overflow: auto;
 }
 </style>
